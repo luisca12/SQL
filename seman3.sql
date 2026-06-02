35eb85124b7fed16f5990ac94ec794afc1c83af8
Luis Alfaro <114428487+luisca12@users.noreply.github.com>
2026-06-02 15:38:50 -0600

    Added Practice from week #3

--- seman3.sql
+++ seman3.sql
@@ -69,3 +69,65 @@ select BT.title, BT.publishDate, AuT.authorName
 from bookT BT, authorT AuT, bookAuthorT BAuT
 where BAuT.authorID = AuT.authorID and BT.bookID = BAuT.bookID
 
+/* Practice Semana 3 */
+create database clinic
+use clinic
+
+create table clinicT(
+    clinicID int primary key,
+    clinicName varchar(50),
+    locaiton varchar(70)
+);
+
+create table clientT(
+    clientID int primary key,
+    clientName varchar(40),
+    clientSurname varchar(40),
+    phoneNum int unique,
+);
+
+create table medT(
+    medID int primary key,
+    medName nvarchar(60),
+    medDesc nvarchar(300),
+    clinicID int,
+
+    Foreign key (clinicID) REFERENCES clinicT(clinicID)
+);
+
+-- This is a shared table, relation many to many
+-- When there is a reltion many to many we MUST have a shared table
+create table medclientT(
+    medID int,
+    clientID int,
+    primary key (medID, clientID),
+    Foreign key (medID) REFERENCES medT(medID),
+    Foreign key (clientID) REFERENCES clientT(clientID)
+);
+
+insert into clinicT values
+(1, 'FarmaValue', 'Purral, San Jose'),
+(2, 'Fischel', 'Guadalupe, San Jose');
+
+insert into clientT values
+(1, 'Luis', 'Alfaro', 90908080),
+(2, 'Jaz', 'Mora', 90907070),
+(3, 'Gloriana', 'Calderon', 80807070),
+(4, 'Nicole', 'Sandoval', 90905050),
+(5, 'Michelle', 'Hernandez', 70704040);
+
+insert into medT values
+(1, 'Paracetamol', 'medicina', 1),
+(2, 'Marihuanol', 'medicina', 2),
+(3, 'Ibuprofeno', 'medicina', 1),
+(4, 'Crema de Rosas', none, 2);
+
+-- Now we need to map the meds to the clients
+
+insert into medclientT values
+(1, 2), -- med with ID 1 has client Jaz
+(1, 3), -- To the same med I can add another client
+(2, 1), -- med with ID 2 has client Luis
+(3, 4), -- med with ID 3 has client Nicole
+(4, 5); -- med with ID 4 has client Michelle
+
